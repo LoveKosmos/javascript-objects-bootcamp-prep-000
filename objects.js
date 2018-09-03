6e153f310130744a5a4ba1223d8347675179d2d7
LoveKosmos <nataliiariabkova@gmail.com>
2018-09-03 22:54:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ function updatePlaylist(playlist, artistName, song){
   return playlist;
 }
  function removeFromPlaylist(playlist,artistName){
-   delete playlist.artistName[song];
+   delete playlist.artistName;
    playlist;
  }
  
